00445fe09ffe8dd14c259e6c07c167d8d72d3b57
Nyraxx <giggletoes42@gmail.com>
2023-05-19 22:04:55 +1000

    a

--- src/spi.c
+++ src/spi.c
@@ -4,28 +4,26 @@
 void spi_init(void){
     cli();
     PORTMUX.SPIROUTEA = PORTMUX_SPI0_ALT1_gc;
-    PORTC.DIR |= (PIN0_bm | PIN2_bm);
+
+    PORTA.OUTCLR = PIN1_bm;
+    PORTA.DIRSET = PIN1_bm;
+
+
+    PORTC.DIRSET = (PIN0_bm | PIN2_bm);
 
     SPI0.CTRLA = SPI_MASTER_bm;
     SPI0.CTRLB = SPI_SSD_bm;
+
+    SPI0.INTCTRL = SPI_IE_bm;
+
     SPI0.CTRLA |= SPI_ENABLE_bm;
 
     sei();
 
-    PORTB.DIRSET = PIN5_bm;
-    PORTA.DIRSET = PIN1_bm;
-    PORTC.DIRSET = PIN0_bm;
-    PORTC.DIRSET = PIN2_bm;
-  
-  //latch stuff
-
-    PORTA.OUTCLR = PIN1_bm;
-    PORTC.OUTCLR = PIN0_bm;
-    PORTC.OUTCLR = PIN2_bm;
-
+ 
 }
 
-int spi_write(uint8_t b){
+void spi_write(uint8_t b){
     SPI0.DATA = b;
     ///interrupt??
     
